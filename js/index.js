0201027fe6438267817dcb463c6bcd0636bca43b
Mehmet Aklanoglu <mehmetaklanoglu@outlook.com>
2023-04-07 15:08:53 +0300

    project end

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const image = document.querySelector(".qr_image"),
+image_caption = document.querySelector(".generate_name"),        
+input = document.querySelector(".generate_site"),
+button = document.querySelector(".gitgenerate_btn"),
+api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=`;
+button.addEventListener("click",() => {
+    image.src = `${api}${input.value}`;
+    image_caption.innerHTML = input.value;
+});
